199a0b554f0f3f07661b52068b1ec6cd7f343fec
Samantha Lilly <samanthalillydesign@gmail.com>
2021-04-27 14:26:34 -0400

    Set up file

---
+++ react_widgets/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+
+  const hello = React.createElement('h1', null, ["hello"])
+
+  ReactDOM.render(hello, root);
+});
