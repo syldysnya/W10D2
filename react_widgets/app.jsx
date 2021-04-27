65f869a7f71d0033b87dea7ca25edbd8775ed7cd
syldysnya <syldysnya@gmail.com>
2021-04-27 14:54:05 -0400

    Started phase 2

--- react_widgets/app.jsx
+++ react_widgets/app.jsx
@@ -4,8 +4,7 @@ import ReactDOM from 'react-dom';
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
 
-  const hello = React.createElement('h1', null, ["hello"])
+  // const hello = React.createElement('h1', null, ["hello"])
 
-  ReactDOM.render(hello, root);
-  console.log('hi')
+  ReactDOM.render(root);
 });
