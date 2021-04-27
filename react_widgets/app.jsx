dfdc6b5f7d26f7cfb3173e49d61e99ae108b0d22
syldysnya <syldysnya@gmail.com>
2021-04-27 14:47:13 -0400

    test

--- react_widgets/app.jsx
+++ react_widgets/app.jsx
@@ -7,4 +7,5 @@ document.addEventListener("DOMContentLoaded", () => {
   const hello = React.createElement('h1', null, ["hello"])
 
   ReactDOM.render(hello, root);
+  console.log('hi')
 });
--- react_widgets/webpack.config.js
+++ react_widgets/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: 'app.jsx',
+    entry: './app.jsx',
     output: {
         path: path.resolve(__dirname),
         filename: 'bundle.js'
